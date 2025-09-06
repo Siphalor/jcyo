c10de034ff7cdc0fe2178808bf7f0707c361b4e4
Siphalor <info@siphalor.de>
2025-09-06 05:30:30 +0200

    fix: Fix unused imports while inside disabled region

--- jcyo-core/src/main/java/de/siphalor/jcyo/core/impl/transform/UnusedImportDisabler.java
+++ jcyo-core/src/main/java/de/siphalor/jcyo/core/impl/transform/UnusedImportDisabler.java
@@ -60,6 +60,7 @@ public class UnusedImportDisabler {
 		var peekableSource = PeekableTokenStream.from(source);
 		return new TokenStream() {
 			private final TokenBuffer buffer = new TokenBuffer();
+			private boolean inDisabledRegion;
 
 			@Override
 			public Token nextToken() {
@@ -68,34 +69,57 @@ public class UnusedImportDisabler {
 				}
 
 				Token token = peekableSource.nextToken();
-				if (token instanceof JavaKeywordToken(JavaKeyword keyword) && keyword == JavaKeyword.IMPORT) {
-					buffer.pushToken(token);
-					boolean used = buffer.copying(peekableSource).stream()
-							.takeWhile(t ->
-									!(t instanceof OperatorToken(int codepoint)) || codepoint == '.' || codepoint == '*'
-							)
-							.map(t -> switch (t) {
-								case IdentifierToken (String identifier) -> identifier;
-								case JavaKeywordToken (JavaKeyword kw) -> kw.name();
-								case OperatorToken (int codepoint) when codepoint == '*' -> Character.toString((char) codepoint);
-								default -> null;
-							})
-							.filter(Objects::nonNull)
-							.reduce((_, second) -> second)
-							.map(t -> t.equals("*") || usedIdentifiers.contains(t))
-							.orElse(false);
-					if (used) {
-						return buffer.nextToken();
-					} else if (peekableSource.peekToken() instanceof LineBreakToken) {
-						buffer.pushToken(peekableSource.nextToken());
-						buffer.pushToken(new JcyoDisabledRegionEndToken());
-						return new JcyoDisabledRegionStartToken(CommentStyle.LINE, "");
-					} else {
-						buffer.pushToken(new JcyoDisabledRegionEndToken());
-						return new JcyoDisabledRegionStartToken(CommentStyle.FLEX, "");
+				switch (token) {
+					case JcyoDisabledRegionStartToken startToken when inDisabledRegion ->
+							throw new IllegalStateException(
+									"Encountered disabled region start token " + startToken +
+											"while already in disabled region"
+							);
+					case JcyoDisabledRegionStartToken _ -> inDisabledRegion = true;
+					case JcyoDisabledRegionEndToken endToken when !inDisabledRegion -> throw new IllegalStateException(
+							"Encountered disabled region end token " + endToken +
+									"while not in disabled region"
+					);
+					case JcyoDisabledRegionEndToken _ -> inDisabledRegion = false;
+					case JavaKeywordToken(JavaKeyword keyword)
+							when !inDisabledRegion && keyword == JavaKeyword.IMPORT -> {
+						buffer.pushToken(token);
+						boolean used = parseImportAndDetermineIfUsed();
+						if (used) {
+							return buffer.nextToken();
+						} else if (peekableSource.peekToken() instanceof LineBreakToken) {
+							buffer.pushToken(peekableSource.nextToken());
+							buffer.pushToken(new JcyoDisabledRegionEndToken());
+							return new JcyoDisabledRegionStartToken(CommentStyle.LINE, "");
+						} else {
+							buffer.pushToken(new JcyoDisabledRegionEndToken());
+							return new JcyoDisabledRegionStartToken(CommentStyle.FLEX, "");
+						}
+					}
+					default -> {
 					}
 				}
 				return token;
+			}
+
+			private boolean parseImportAndDetermineIfUsed() {
+				return buffer.copying(peekableSource).stream()
+						.takeWhile(t ->
+								!(t instanceof OperatorToken(int codepoint))
+										|| codepoint == '.'
+										|| codepoint == '*'
+						)
+						.map(t -> switch (t) {
+							case IdentifierToken(String identifier) -> identifier;
+							case JavaKeywordToken(JavaKeyword kw) -> kw.name();
+							case OperatorToken(int codepoint) when codepoint == '*' ->
+									Character.toString((char) codepoint);
+							default -> null;
+						})
+						.filter(Objects::nonNull)
+						.reduce((_, second) -> second)
+						.map(t -> t.equals("*") || usedIdentifiers.contains(t))
+						.orElse(false);
 			}
 		};
 	}
