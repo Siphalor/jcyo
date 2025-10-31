e16f7056deef33d2f1dba08d4223395cd73c3b59
Siphalor <info@siphalor.de>
2025-10-31 13:51:40 +0100

    feat: Only update files when they have changed

--- jcyo-core/src/main/java/de/siphalor/jcyo/core/impl/JcyoProcessor.java
+++ jcyo-core/src/main/java/de/siphalor/jcyo/core/impl/JcyoProcessor.java
@@ -9,7 +9,14 @@ import de.siphalor.jcyo.core.impl.transform.*;
 import org.jspecify.annotations.Nullable;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.DigestInputStream;
+import java.security.DigestOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class JcyoProcessor {
 	private final JcyoOptions options;
@@ -50,24 +57,45 @@ public class JcyoProcessor {
 	}
 
 	void processFile(File input, @Nullable File cleanOutput) throws JcyoProcessingException {
-		TokenStream processedTokenStream = getProcessedTokensStreamForFile(input);
-
-		if (options.updateInput()) {
-			if (cleanOutput == null) {
-				writeToFile(input, processedTokenStream);
-			} else {
-				TokenBuffer copy = new TokenBuffer();
-				writeToFile(input, copy.copying(processedTokenStream));
-				writeToFile(cleanOutput, new JcyoCleaner(copy));
+		InputStream inputStream = null;
+		try {
+			MessageDigest digest = null;
+			inputStream = new FileInputStream(input);
+			if (options.updateInput()) {
+				digest = MessageDigest.getInstance("SHA-1");
+				inputStream = new DigestInputStream(inputStream, digest);
 			}
-		} else {
-			assert cleanOutput != null;
-			writeToFile(cleanOutput, processedTokenStream);
+
+			TokenStream processedTokenStream = getProcessedTokensStreamForFile(
+					new InputStreamReader(inputStream, StandardCharsets.UTF_8)
+			);
+
+			if (options.updateInput()) {
+				if (cleanOutput == null) {
+					updateFileOnChanged(input, digest, processedTokenStream);
+				} else {
+					TokenBuffer copy = new TokenBuffer();
+					updateFileOnChanged(input, digest, copy.copying(processedTokenStream));
+					writeToFile(cleanOutput, new JcyoCleaner(copy));
+				}
+			} else {
+				assert cleanOutput != null;
+				writeToFile(cleanOutput, processedTokenStream);
+			}
+		} catch (IOException e) {
+			throw new JcyoProcessingException("Failed to read input file: " + input, e);
+		} catch (NoSuchAlgorithmException e) {
+			throw new JcyoProcessingException("Failed to initialize SHA-1 digest", e);
+		} finally {
+			assert inputStream != null;
+			try {
+				inputStream.close();
+			} catch (IOException ignored) {}
 		}
 	}
 
-	TokenStream getProcessedTokensStreamForFile(File input) throws JcyoProcessingException {
-		try (var lexer = new JcyoLexer(new BufferedReader(new FileReader(input)), options)) {
+	TokenStream getProcessedTokensStreamForFile(Reader input) throws JcyoProcessingException {
+		try (var lexer = new JcyoLexer(input, options)) {
 
 			TokenStream streamWithOldStuffRemoved = new GeneratedAndDisabledTokenRemover(
 					new JcyoUnpadder(lexer),
@@ -76,12 +104,55 @@ public class JcyoProcessor {
 			TokenStream streamWithDirectivesApplied = directiveApplier.apply(streamWithOldStuffRemoved);
 			return unusedImportDisabler.apply(streamWithDirectivesApplied);
 
-		} catch (FileNotFoundException e) {
-			throw new JcyoProcessingException("Failed to read input file: " + input, e);
 		} catch (JcyoParseException e) {
 			throw new JcyoProcessingException("Failed to parse input file: " + input, e);
 		} catch (Exception e) {
 			throw new JcyoProcessingException("Unexpected exception for input file: " + input, e);
+		}
+	}
+
+	void updateFileOnChanged(File file, MessageDigest oldDigest, TokenStream tokenStream) throws JcyoProcessingException {
+		File tempFile = createTempFileForOutput(file);
+
+		MessageDigest newDigest;
+		try {
+			newDigest = MessageDigest.getInstance(oldDigest.getAlgorithm());
+		} catch (NoSuchAlgorithmException e) {
+			throw new JcyoProcessingException("Missing digest algorithm: " + oldDigest.getAlgorithm(), e);
+		}
+
+		try (var outputStream = new DigestOutputStream(new FileOutputStream(tempFile), newDigest)) {
+			try (var writer = new TokenWriter(
+					new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)),
+					options
+			)) {
+				writer.writeAll(tokenStream);
+			}
+		} catch (IOException e) {
+			throw new JcyoProcessingException("Failed to write to temporary output file: " + tempFile, e);
+		}
+
+		if (!Arrays.equals(newDigest.digest(), oldDigest.digest())) {
+			if (!file.delete()) {
+				throw new JcyoProcessingException("Failed to delete file: " + file);
+			}
+			try {
+				Files.move(tempFile.toPath(), file.toPath());
+			} catch (IOException e) {
+				throw new JcyoProcessingException(
+						"Failed to move temporary output file: " + tempFile + " to: " + file, e
+				);
+			}
+		} else {
+			tempFile.deleteOnExit();
+		}
+	}
+
+	private File createTempFileForOutput(File file) throws JcyoProcessingException {
+		try {
+			return Files.createTempFile("jcyo-processor", file.getName()).toFile();
+		} catch (IOException e) {
+			throw new JcyoProcessingException("Failed to create temporary file", e);
 		}
 	}
 
